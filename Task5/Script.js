0fd424cf59211713d2ca4e69a74ee9476adddcb8
Neetu Seharawat <neetumorbca@gmail.com>
2022-11-14 13:00:31 +0530

    Task 5 added

---
+++ Task5/Script.js
@@ -0,0 +1,11 @@
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title); //document.title=123; change name also
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body); 
+console.log(document.all);
+console.log(document.all[10]);
+
+
+console.log(document.getElementById("header-title"));
